546dfcf8e8979f932dc247c5e43553009863cbd5
Rafael Passos <rapassos@gmail.com>
2026-02-18 15:23:11 -0300

    Initial commit

---
+++ src/main/java/com/rapassos/tasks_api_java/TasksApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rapassos.tasks_api_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapassos/tasks_api_java/TasksApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapassos.tasks_api_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
